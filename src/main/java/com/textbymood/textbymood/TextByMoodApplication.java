8d2cc8127ca03a17de41ff4870cd524f12481dba
Justin Singh <justinsingh2015@gmail.com>
2020-10-09 18:13:36 -0700

    first commit

---
+++ src/main/java/com/textbymood/textbymood/TextByMoodApplication.java
@@ -0,0 +1,13 @@
+package com.textbymood.textbymood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextByMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextByMoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/textbymood/textbymood/TextByMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textbymood.textbymood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextByMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
