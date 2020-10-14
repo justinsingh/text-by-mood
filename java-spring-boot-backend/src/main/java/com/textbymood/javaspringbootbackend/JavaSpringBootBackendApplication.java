47bd67c93a512bae3966839b9adc02df1d1c453a
Justin Singh <justinsingh2015@gmail.com>
2020-10-13 18:40:46 -0700

    Initialize react-frontend

--- java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/JavaSpringBootBackendApplication.java
+++ java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/JavaSpringBootBackendApplication.java
@@ -1,13 +1,13 @@
-package com.textbymood.app;
+package com.textbymood.javaspringbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class JavaSpringBootBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(JavaSpringBootBackendApplication.class, args);
 	}
 
 }
--- java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/JavaSpringBootBackendApplicationTests.java
+++ java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/JavaSpringBootBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.textbymood.app;
+package com.textbymood.javaspringbootbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppApplicationTests {
+class JavaSpringBootBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
