a038092af472c13ad517ea831bb802257aaaec14
Justin Singh <justinsingh2015@gmail.com>
2020-10-21 16:12:13 -0700

    Add JUnit tests

--- java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/LexiconTest.java
+++ java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/LexiconTest.java
@@ -3,6 +3,8 @@ package com.textbymood.javaspringbootbackend;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,5 +20,17 @@ public class LexiconTest {
         assertEquals(true, vocabulary.get("surprise").contains("abruptly"));
 
         assertEquals(false, lexicon.addWord("abruptly", "surprise"));
+    }
+
+    @Test
+    void testAddingWordsFromFile() {
+        File testFile = new File("src/test/java/com/textbymood/javaspringbootbackend/LexiconTextTest.txt");
+        lexicon.addWordsFromFile(testFile); 
+
+        assertEquals(true, vocabulary.get("fear").contains("abandon"));
+        assertEquals(true, vocabulary.get("fear").contains("abandonment"));
+        assertEquals(false, vocabulary.get("fear").contains("abacus"));
+        assertEquals(true, vocabulary.get("positive").contains("abba"));
+        assertEquals(false, vocabulary.get("positive").contains("abhorrent"));
     }
 }
