b9ce87721e2543839887d2c44a4f13325677124f
Justin Singh <justinsingh2015@gmail.com>
2020-11-12 00:05:36 -0800

    Modify Dependcies

--- java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/controllers/EmotionLexiconController.java
+++ java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/controllers/EmotionLexiconController.java
@@ -1,10 +1,9 @@
 package com.textbymood.javaspringbootbackend.controllers;
 
 import java.io.File;
-import java.util.Map;
-import java.util.Set;
 
 import com.textbymood.javaspringbootbackend.models.Lexicon;
+import com.textbymood.javaspringbootbackend.models.TextAnalyzer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmotionLexiconController {
 
     private static final File emotionLexiconWordFile = new File("java-spring-boot-backend/src/main/resources/static/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt");
-    private static final Lexicon EmotionLexicon = new Lexicon(emotionLexiconWordFile);
-    private static final Map<String, Set<String>> emotionVocabulary = EmotionLexicon.getVocabulary();
+    private static final Lexicon emotionLexicon = new Lexicon(emotionLexiconWordFile);
+    private static final TextAnalyzer emotionTextAnalyzer = new TextAnalyzer(emotionLexicon);
 
-    @GetMapping("/getEmotionVocabulary")
-    public Map<String, Set<String>> getEmotionVocabulary() {
-        return emotionVocabulary;
+    @GetMapping("/getEmotionLexicon")
+    public Lexicon getEmotionVocabulary() {
+        return emotionLexicon;
     } 
     
+
 }
--- java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/models/Lexicon.java
+++ java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/models/Lexicon.java
@@ -1,7 +1,6 @@
 package com.textbymood.javaspringbootbackend.models;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.HashMap;
