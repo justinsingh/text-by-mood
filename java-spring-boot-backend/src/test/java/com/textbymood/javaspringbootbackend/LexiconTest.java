b2c58705d66dfb81cb4ad2ee4415b4ace32f4335
Justin Singh <justinsingh2015@gmail.com>
2020-11-15 01:53:49 -0800

    Add stop word tests to LexiconTest.java

--- java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/LexiconTest.java
+++ java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/LexiconTest.java
@@ -2,6 +2,8 @@ package com.textbymood.javaspringbootbackend;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.util.Map;
@@ -10,26 +12,43 @@ import java.util.Set;
 import com.textbymood.javaspringbootbackend.models.Lexicon;
 
 public class LexiconTest {
-    Lexicon lexicon = new Lexicon(); 
+    Lexicon lexicon = new Lexicon();
     Map<String, Set<String>> vocabulary = lexicon.getVocabulary();
+    Set<String> stopWords = lexicon.getStopWords();
 
     @Test
     void testAddingWord() {
         lexicon.addWord("abruptly", "surprise");
-        assertEquals(true, vocabulary.get("surprise").contains("abruptly"));
-
-        assertEquals(false, lexicon.addWord("abruptly", "surprise"));
+        assertTrue(vocabulary.get("surprise").contains("abruptly"));
+        assertFalse(lexicon.addWord("abruptly", "surprise"));
     }
 
     @Test
     void testAddingWordsFromFile() {
-        File testFile = new File("src/test/java/com/textbymood/javaspringbootbackend/LexiconTextTest.txt");
-        lexicon.addWordsFromFile(testFile); 
+        File wordFile = new File("src/test/java/com/textbymood/javaspringbootbackend/LexiconTextTest.txt");
+        lexicon.addWordsFromFile(wordFile);
 
-        assertEquals(true, vocabulary.get("fear").contains("abandon"));
-        assertEquals(true, vocabulary.get("fear").contains("abandonment"));
-        assertEquals(false, vocabulary.get("fear").contains("abacus"));
-        assertEquals(true, vocabulary.get("positive").contains("abba"));
-        assertEquals(false, vocabulary.get("positive").contains("abhorrent"));
+        assertTrue(vocabulary.get("fear").contains("abandon"));
+        assertTrue(vocabulary.get("fear").contains("abandonment"));
+        assertFalse(vocabulary.get("fear").contains("abacus"));
+        assertTrue(vocabulary.get("positive").contains("abba"));
+        assertFalse(vocabulary.get("positive").contains("abhorrent"));
+    }
+
+    @Test
+    void testAddingStopWord() {
+        lexicon.addStopWord("and");
+        assertTrue(stopWords.contains("and"));
+        assertFalse(stopWords.contains("negative"));
+    }
+
+    @Test
+    void testAddingStopWordsFromFile() {
+        File stopWordFile = new File("src/test/java/com/textbymood/javaspringbootbackend/LexiconStopWordsTest.txt"); 
+        lexicon.addStopWordsFromFile(stopWordFile);
+
+        assertTrue(stopWords.contains("or"));
+        assertTrue(stopWords.contains("have"));
+        assertFalse(stopWords.contains("positive"));
     }
 }
