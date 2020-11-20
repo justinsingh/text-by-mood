0ff63722a360d8dd3583e09491c5c6e54867f7d3
Justin Singh <justinsingh2015@gmail.com>
2020-11-20 03:30:25 -0800

    Refactor and implement TextAnalyzer.java methods

--- java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/models/TextAnalyzer.java
+++ java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/models/TextAnalyzer.java
@@ -3,11 +3,12 @@ package com.textbymood.javaspringbootbackend.models;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * TextAnalyzer.java --- A class for scanning bodies of text to determine their relationships to a lexicon.
@@ -24,20 +25,39 @@ public class TextAnalyzer {
     }
 
     public TextAnalysisResult analyzeText(String text) {
-        String[] words = splitTextToWords(text);
+        String[] words = createArrayOfNonStopWords(text);
 
-        Map<String, Set<String>> descriptiveWordsMap = createDescriptiveWordsMap(words);
-        Map<String, Integer> descriptorScoreMap = createDescriptorScoreMap(descriptiveWordsMap, words);
-        List<String> topDescriptors = createTopDescriptorsList(descriptorScoreMap);
+        Map<String, List<String>> descriptorToListOfWords = createDescriptorToListOfWordsMap(words);
+        Map<String, Set<String>> descriptorToSetOfWords = createDescriptorToSetOfWordsMap(descriptorToListOfWords);
+        Map<String, Integer> descriptorToScore = createDescriptorToScoreMap(descriptorToListOfWords, words.length);
+        List<String> topDescriptors = createTopDescriptorsList(descriptorToScore);
         
-        return new TextAnalysisResult(text, topDescriptors, descriptorScoreMap, descriptiveWordsMap);
+        return new TextAnalysisResult(text, topDescriptors, descriptorToScore, descriptorToSetOfWords);
     }
 
-    private String[] splitTextToWords(String text) {
+    private String[] createArrayOfNonStopWords(String text) {
         return Arrays.stream(text.split("\\s+"))
                      .map(word -> word.replaceAll("[^a-zA-Z ]", ""))
-                     .map(word -> word.toLowerCase()).filter(word -> !lexicon.getStopWords().contains(word))
+                     .map(word -> word.toLowerCase())
+                     .filter(word -> !lexicon.getStopWords().contains(word))
                      .toArray(String[]::new);
+    }
+
+    private Map<String, List<String>> createDescriptorToListOfWordsMap(String[] words) {
+        Map<String, List<String>> descriptorToListOfWords = new HashMap<String, List<String>>();
+
+        Arrays.stream(words)
+              .forEach(word -> addWord(word, createDescriptorsOfWordList(word), descriptorToListOfWords));
+
+        return descriptorToListOfWords;
+    }
+
+    private void addWord(String word, List<String> descriptorsOfWord, Map<String, List<String>> descriptiveWordsMap) {
+        descriptorsOfWord.stream()
+                         .forEach(descriptor -> {
+                             descriptiveWordsMap.putIfAbsent(descriptor, new ArrayList<String>());
+                             descriptiveWordsMap.get(descriptor).add(word);
+                         });
     }
 
     private List<String> createDescriptorsOfWordList(String word) {
@@ -46,29 +66,35 @@ public class TextAnalyzer {
                                   .collect(Collectors.toList());
     }
 
-    private void addWordToDescriptiveWordsMap(String word, List<String> descriptorsOfWord, Map<String, Set<String>> descriptiveWordsMap) {
-        descriptorsOfWord.stream()
-                         .forEach(descriptor -> {
-                             descriptiveWordsMap.putIfAbsent(descriptor, new Set<String>());
-                             descriptiveWordsMap.get(descriptor).add(word);
-                         });
+    private Map<String, Set<String>> createDescriptorToSetOfWordsMap(Map<String, List<String>> descriptorToListOfWords) {
+        Map<String, Set<String>> descriptorToSetOfWords = new HashMap<String, Set<String>>();
+
+        for (String descriptor: descriptorToListOfWords.keySet()) {
+            descriptorToSetOfWords.put(descriptor, new HashSet<String>(descriptorToListOfWords.get(descriptor)));
+        }
+
+        return descriptorToSetOfWords;
     }
 
-    private Map<String, Set<String>> createDescriptiveWordsMap(String[] words) {
-        Map<String, Set<String>> descriptiveWordsMap = new HashMap<String, Set<String>>();
+    private Map<String, Integer> createDescriptorToScoreMap(Map<String, List<String>> descriptorToListOfWords, int numOfWords) {
+        Map<String, Integer> descriptorToScore = new HashMap<String, Integer>();
 
-        Arrays.stream(words)
-              .forEach(word -> addWordToDescriptiveWordsMap(word, createDescriptorsOfWordList(word), descriptiveWordsMap));
+        for (String descriptor: descriptorToListOfWords.keySet()) {
+            int listOfWordsLength = descriptorToListOfWords.get(descriptor).size();
+            descriptorToScore.put(descriptor, (listOfWordsLength / numOfWords) * 100);
+        }
 
-        return descriptiveWordsMap;
+        return descriptorToScore;
     }
 
-    private Map<String, Integer> createDescriptorScoreMap(Map<String, Set<String>> descriptiveWordsMap, String[] words) {
-        return new HashMap<String, Integer>();
-    }
+    private List<String> createTopDescriptorsList(Map<String, Integer> descriptorToScore) {
+        Optional<Integer> highestDescriptorScoreOptional = descriptorToScore.values().stream()
+                                                               .max((num1, num2) -> num1.compareTo(num2));
+        int highestDescriptorScore = highestDescriptorScoreOptional.orElse(-1);
 
-    private List<String> createTopDescriptorsList(Map<String, Integer> descriptorScoreMap) {
-        return new ArrayList<String>();
+        return descriptorToScore.keySet().stream()
+                                .filter(descriptor -> descriptorToScore.get(descriptor) == highestDescriptorScore)
+                                .collect(Collectors.toList());
     }
 
 }
