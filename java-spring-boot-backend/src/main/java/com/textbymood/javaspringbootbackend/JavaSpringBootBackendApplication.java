1743b62c6333e94e79d444a24bee079ed2200420
Justin Singh <justinsingh2015@gmail.com>
2020-11-21 19:49:06 -0800

    Remove Spring Boot Security Auto Configuration

--- java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/JavaSpringBootBackendApplication.java
+++ java-spring-boot-backend/src/main/java/com/textbymood/javaspringbootbackend/JavaSpringBootBackendApplication.java
@@ -2,12 +2,12 @@ package com.textbymood.javaspringbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class JavaSpringBootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringBootBackendApplication.class, args);
 	}
-
 }
