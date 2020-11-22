29b3a7f74cb67044a8d29e815897e7f7bbfb7570
Justin Singh <justinsingh2015@gmail.com>
2020-11-21 19:53:25 -0800

    Remove dependency

--- java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/LexiconTest.java
+++ java-spring-boot-backend/src/test/java/com/textbymood/javaspringbootbackend/LexiconTest.java
@@ -1,7 +1,6 @@
 package com.textbymood.javaspringbootbackend;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
